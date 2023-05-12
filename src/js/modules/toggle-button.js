90efc218572c27e2a2f19305c8895d3d2922df78
Iraida <ira.serq.1996@gmail.com>
2023-05-12 15:22:24 +0300

    исправила функцию

--- src/js/modules/toggle-button.js
+++ src/js/modules/toggle-button.js
@@ -1,7 +1,26 @@
 export function toggle () {
     let button = document.querySelector('.toggle__button')
+    let monthlyPriceList = document.querySelectorAll('.monthly-price')
+    let yearlyPriceList = document.querySelectorAll('.yearly-price')
     
     button.addEventListener('click', function () {
-        button.classList.toggle("toggle_active");
+        let buttonState = button.classList.toggle("toggle_active");
+
+        if (buttonState == true) {
+            for (let i = 0; i < monthlyPriceList.length; i++) {                
+                monthlyPriceList[i].style.opacity = "0"
+                monthlyPriceList[i].style.display = "none"
+                yearlyPriceList[i].style.opacity = "1"
+                yearlyPriceList[i].style.display = "flex"
+            }
+        }
+        else {
+            for (let i = 0; i < monthlyPriceList.length; i++) {                
+                monthlyPriceList[i].style.opacity = "1"
+                monthlyPriceList[i].style.display = "flex"
+                yearlyPriceList[i].style.opacity = "0"
+                yearlyPriceList[i].style.display = "none"
+            }
+        }
     })
 }
