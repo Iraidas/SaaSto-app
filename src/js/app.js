0ffcb20d63ea26175bbda896ccd217c4d13c71fe
Iraida <ira.serq.1996@gmail.com>
2023-06-16 17:25:59 +0300

    добавила слайдер

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,20 @@ import * as toggle from "./modules/toggle-button.js";
 flsFunctions.isWebp();
 menuFunctions.burgerMenu();
 toggle.toggle();
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+Swiper.use([Navigation, Pagination]);
+
+const slider = new Swiper('.swiper', {
+    loop: true,
+    width: 900,
+    slidesPerView: 1,
+    centeredSlides: true,
+    initialSlide: 0,
+    spaceBetween: 100,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true, 
+    },
+});
