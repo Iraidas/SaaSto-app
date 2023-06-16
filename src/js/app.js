0ec0d2a8d95912b3808ab7c59cf93ad4fed69f96
Iraida <ira.serq.1996@gmail.com>
2023-06-16 21:20:17 +0300

    стили для мобильной версии

--- src/js/app.js
+++ src/js/app.js
@@ -11,7 +11,6 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 Swiper.use([Navigation, Pagination]);
 
 const slider = new Swiper('.swiper', {
-    loop: true,
     width: 900,
     slidesPerView: 1,
     centeredSlides: true,
@@ -21,4 +20,21 @@ const slider = new Swiper('.swiper', {
         el: '.swiper-pagination',
         clickable: true, 
     },
+    breakpoints: {
+        1000: {
+            width: 900,
+        },
+        800: {
+            width: 700,
+        },
+        600: {
+            width: 500,
+        },
+        440: {
+            width: 360,
+        },
+        280: {
+            width: 280,
+        }
+    }
 });
